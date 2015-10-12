2dd536705132650dbcfff23a67bc298af7032bcc
Ozitiho <bvkoesveld@gmail.com>
2015-10-12 10:14:30 +0200

    Updated gitignore + Staring scripts

---
+++ DK2_EggShellVR/Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Transform View;
+
+	void Start () 
+	{
+		View = FindTransform (transform, "CenterEyeAnchor");
+	}
+
+	//Gets the angle at which the player is looking at a position
+	public float GetAngle(Vector3 pos)
+	{
+		Vector3 dir = pos - transform.position;
+		float angle = Vector3.Angle (View.forward, dir);
+		return angle;
+	}
+
+	//Find a child object by name
+	public static Transform FindTransform(Transform parent, string name)
+	{
+		if (parent.name.Equals(name))
+			return parent;
+		foreach (Transform child in parent)
+		{
+			Transform result = FindTransform(child, name);
+			if (result != null) return result;
+		}
+		return null;
+	}
+}
