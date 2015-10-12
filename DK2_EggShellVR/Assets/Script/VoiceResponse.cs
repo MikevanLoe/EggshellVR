16031c3e7bfc2495afa9fcdf8a137631f9231ef7
Mike <mikevanloe@gmail.com>
2015-10-12 16:21:22 +0200

    Response 1.2

--- DK2_EggShellVR/Assets/Script/VoiceResponse.cs
+++ DK2_EggShellVR/Assets/Script/VoiceResponse.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 
 public class VoiceResponse : MonoBehaviour {
-	// Volume tussen 0 en 1
 	// Punten gebaseerd op jou tijd en de 'stocktijd'
 
-	private float micIn;
+	private float micIn;	
 	private float maxMicIn;
 	private float minMicIn;
 	private float score;
 
 	void Update()
 	{
+		//Calculate the highest and lowest input
 		maxMicIn = Mathf.Max(maxMicIn, micIn);
 		minMicIn = Mathf.Min (minMicIn, micIn);
 
+		//If the current input is loud enough, increase the score
 		if (micIn > ( (maxMicIn + minMicIn) / 2))
 		{
 			score += 10;
