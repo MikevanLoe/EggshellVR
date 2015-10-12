1338457dd660bceacaef86e000c856205d2527ea
Mike <mikevanloe@gmail.com>
2015-10-12 14:22:57 +0200

    Response v1

--- DK2_EggShellVR/Assets/Script/VoiceResponse.cs
+++ DK2_EggShellVR/Assets/Script/VoiceResponse.cs
@@ -2,7 +2,20 @@
 using System.Collections;
 
 public class VoiceResponse : MonoBehaviour {
+	// Volume tussen 0 en 1
+	// Punten gebaseerd op jou tijd en de 'stocktijd'
 
-	private int volume;
+	private var micIn;
+	private var maxMicIn;
+	private int score;
 
+	void Update()
+	{
+		maxMicIn = Mathf.Max(maxMicIn, micIn);
+
+		if (micIn > maxMicIn/2)
+		{
+			score += 10;
+		}
+	}
 }
