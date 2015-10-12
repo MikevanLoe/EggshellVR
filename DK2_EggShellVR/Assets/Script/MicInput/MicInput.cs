0747804cf28064b0fd9697308339ba60ded567b4
Ozitiho <bvkoesveld@gmail.com>
2015-10-12 14:25:14 +0200

    Started mic input

---
+++ DK2_EggShellVR/Assets/Script/MicInput/MicInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using NAudio.Wave;
+
+public class MicInput {
+	private WaveInEvent waveIn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
