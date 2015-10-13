396240540d6906eff84a74fb44d52f9229d64ccb
Ozitiho <bvkoesveld@gmail.com>
2015-10-13 16:03:25 +0200

    finished code review

--- DK2_EggShellVR/Assets/Script/MicrophoneInput.cs
+++ DK2_EggShellVR/Assets/Script/MicrophoneInput.cs
@@ -12,7 +12,8 @@ public class MicrophoneInput
 		Microphone.GetDeviceCaps (Microphone.devices[0], out _sampleRateMin, out max);
 		//If the sample rate is 0, that means it supports any rate. 
 		//In this case, set it to the default 44100.
-		if (_sampleRateMin == 0) {
+		if (_sampleRateMin == 0) 
+		{
 			_sampleRateMin = 44100;
 		}
 	}
@@ -33,7 +34,8 @@ public class MicrophoneInput
 		//Get the avarage value of all samples in the current recording
 		int count = 0;
 		float avg = 0f;
-		for (int i = 0; i < _micClip.samples; i++) {
+		for (int i = 0; i < _micClip.samples; i++) 
+		{
 			count++;
 			//Add the absolute of the current value to the avarage calculation
 			avg = avg + (Math.Abs(samples[i]) - avg) / count;
