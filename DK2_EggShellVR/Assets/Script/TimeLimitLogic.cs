7caa314362aa7079fe79a13fcc673ee6bcf37b51
Mike <mikevanloe@gmail.com>
2015-10-27 16:24:52 +0100

    Temporary version for TimeLimit

--- DK2_EggShellVR/Assets/Script/TimeLimitLogic.cs
+++ DK2_EggShellVR/Assets/Script/TimeLimitLogic.cs
@@ -1,9 +1,21 @@
 using System;
-public class TimeLimitLogic
+using UnityEngine;
+
+public class TimeLimitLogic : MonoBehaviour
 {
+	public Sentence sentence;
 
-	public TimeLimitLogic ()
+	private float startTime;
+	private float takenTime;
+
+	public TimeLimitLogic (Sentence activeSentence)
 	{
+		sentence = activeSentence;
+		startTime = Time.time;
+	}
 
+	void Update()
+	{
+		takenTime = Time.time - startTime;
 	}
 }
