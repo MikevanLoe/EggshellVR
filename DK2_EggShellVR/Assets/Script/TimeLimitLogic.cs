7b71b2d7afccbd91a0991b5696158b36c202d280
Mike <mikevanloe@gmail.com>
2015-10-30 17:00:35 +0100

    V 0.9, Minor tweaks needed till completion

--- DK2_EggShellVR/Assets/Script/TimeLimitLogic.cs
+++ DK2_EggShellVR/Assets/Script/TimeLimitLogic.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 public class TimeLimitLogic : MonoBehaviour
 {
 	public Sentence sentence;
+	public Transform baseTime;
+	public Transform takenTime;
+	public Transform overTime;
 
 	private float startTime;
-	private float takenTime;
+	private float activeTime;
+	private Sentence zin = new Sentence("Test zin", 5.0f);
 
-	public TimeLimitLogic (Sentence activeSentence)
+	void Start(/*Sentence activeSentence*/)
 	{
-		sentence = activeSentence;
+		sentence = zin; //activeSentence;
 		startTime = Time.time;
 	}
 
 	void Update()
 	{
-		takenTime = Time.time - startTime;
+		activeTime = Time.time - startTime;
+
+		if (activeTime < sentence.time)
+		{
+			GameObject takenObj = takenTime.gameObject;
+			takenObj.SetActive(true);
+			float xTransform = 0.8f * ((1/sentence.time) * activeTime);
+			takenTime.localScale = new Vector3(xTransform, 0.1f, 0.001f);
+		}
+		else if(activeTime >= sentence.time)
+		{
+			baseTime.gameObject.SetActive(false);
+			overTime.gameObject.SetActive(true);
+			float xTransform = 0.8f * (((1/sentence.time) * activeTime)-1);
+			overTime.localScale = new Vector3(xTransform, 0.1f, 0.001f);
+		}
 	}
 }
