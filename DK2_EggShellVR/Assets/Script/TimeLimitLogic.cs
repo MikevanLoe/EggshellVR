74b6a7c699bc4a1cf8499701e52d9b4f50aed511
Mike <mikevanloe@gmail.com>
2015-11-02 10:33:45 +0100

    Updated and reviewed

--- DK2_EggShellVR/Assets/Script/TimeLimitLogic.cs
+++ DK2_EggShellVR/Assets/Script/TimeLimitLogic.cs
@@ -3,42 +3,41 @@ using UnityEngine;
 
 public class TimeLimitLogic : MonoBehaviour
 {
-	public Sentence sentence;
+	const float YSizeTimer = 0.1f;
+	const float ZSizeTimer = 0.001f;
+	const float MaxXTimer = 0.8f;
+
+	public float sentence;
 	public Transform baseTime;
 	public Transform takenTime;
 	public Transform overTime;
 
 	private float startTime;
-	private float activeTime;
 	private Sentence zin = new Sentence("Test zin", 5.0f);
 
 	void Start(/*Sentence activeSentence*/)
 	{
-		sentence = zin; //activeSentence;
+		sentence = zin.time; //activeSentence;
 		startTime = Time.time;
 	}
 
 	void Update()
 	{
-		activeTime = Time.time - startTime;
+		float activeTime = Time.time - startTime;
 
-		if (activeTime >= 2*sentence.time)
-		{
-
-		}
-		else if (activeTime >= sentence.time)
+		if (activeTime <= 2*sentence)
 		{
 			baseTime.gameObject.SetActive (false);
 			overTime.gameObject.SetActive (true);
-			float xTransform = 0.8f * (((1 / sentence.time) * activeTime) - 1);
-			overTime.localScale = new Vector3 (xTransform, 0.1f, 0.001f);
-		} 
-		else if (activeTime < sentence.time)
+			float xTransform = MaxXTimer * (((1 / sentence) * activeTime) - 1);
+			overTime.localScale = new Vector3 (xTransform, YSizeTimer, ZSizeTimer);
+		}
+		else if (activeTime < sentence)
 		{
 			GameObject takenObj = takenTime.gameObject;
 			takenObj.SetActive (true);
-			float xTransform = 0.8f * ((1 / sentence.time) * activeTime);
-			takenTime.localScale = new Vector3 (xTransform, 0.1f, 0.001f);
+			float xTransform = MaxXTimer * ((1 / sentence) * activeTime);
+			takenTime.localScale = new Vector3 (xTransform, YSizeTimer, ZSizeTimer);
 		}
 	}
 }
