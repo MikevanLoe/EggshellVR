f037a38bffe622b173038ada6dcd5a734d9c5c6e
Ozitiho <bvkoesveld@gmail.com>
2015-11-12 11:41:07 +0100

    Heartrate everything

---
+++ DK2_EggShellVR/Assets/Script/Controller/MenuController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public CraftingState craftingState;
+
+	[HideInInspector]
+	public PlayerController Player;
+
+	private StateMachine<MenuController> _stateMachine;
+	[SerializeField]
+	private Transform SelectionHighlight;
+
+	// Use this for initialization
+	void Start () {
+		Player = transform.parent.GetComponent<PlayerController>();
+		_stateMachine = new StateMachine<MenuController> ();
+
+		craftingState.Start ();
+
+		_stateMachine.Add (craftingState);
+
+		_stateMachine.Set ("CraftingState");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_stateMachine.Handle ();
+	}
+	
+	public void SetSelectionPosition(Transform t)
+	{
+		SelectionHighlight.position = t.position;
+		SelectionHighlight.rotation = t.rotation;
+	}
+	
+	public void SetSelectionPosition(Vector3 pos)
+	{
+		SelectionHighlight.position = pos;
+	}
+	
+	public void SetSelectionPosition(Vector3 pos, Quaternion rot)
+	{
+		SelectionHighlight.position = pos;
+		SelectionHighlight.rotation = rot;
+	}
+}
---
+++ DK2_EggShellVR/Assets/Script/TagDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TagDetector : MonoBehaviour {
+	public string Tag;
+	public List<GameObject> Listeners;
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == Tag) {
+			Listeners.ForEach(l => l.SendMessage("DetectOn", SendMessageOptions.DontRequireReceiver));
+		}
+	}
+	
+	void OnTriggerExit(Collider other) {
+		if (other.tag == Tag) {
+			Listeners.ForEach(l => l.SendMessage("DetectOff", SendMessageOptions.DontRequireReceiver));
+		}
+	}
+}
