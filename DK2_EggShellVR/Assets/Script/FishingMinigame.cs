93a16ec162b53d8e9dfb672b548dad3541535bfd
Mike <mikevanloe@gmail.com>
2015-11-16 08:17:21 +0100

    v0.5 Added Wander()

--- DK2_EggShellVR/Assets/Script/FishingMinigame.cs
+++ DK2_EggShellVR/Assets/Script/FishingMinigame.cs
@@ -6,6 +6,13 @@ public class FishingMinigame : MonoBehaviour
 	public bool isRodActive;
 	public bool isHookOut;
 
+	public float WanderRadius;
+	public float WanderDistance;
+	public float WanderJitter;
+	public Vector2 Heading;
+	
+	private Vector2 WanderTarget;
+
 	public void Start ()
 	{
 		LockMovement ();
@@ -23,9 +30,36 @@ public class FishingMinigame : MonoBehaviour
 		// Code van Bas
 	}
 
-	public void SpawnFish()
+	public void SpawnFish ()
 	{
 
+	}
+
+	public void MoveFish ()
+	{
+
+	}
+
+	public Vector2 Wander()
+	{
+		//first, add a small random vector to the target’s position (RandomClamped
+		//returns a value between -1 and 1)
+		System.Random randomX = new System.Random ();
+		System.Random randomZ = new System.Random ();
+
+		WanderTarget += new Vector2 (randomX.Next(-1, 2) * WanderJitter,
+		                             randomZ.Next(-1, 2) * WanderJitter);
+		
+		//reproject this new vector back onto a unit circle
+		WanderTarget.Normalize ();
+		
+		//increase the length of the vector to the same as the radius
+		//of the wander circle
+		WanderTarget *= WanderRadius;
+		
+		//move the target into a position WanderDist in front of the agent
+		Vector2 WanderForce = Heading + WanderTarget;
+		return WanderForce;
 	}
 
 	public void ActivateRod ()
