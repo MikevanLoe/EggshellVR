1c920784f79aad26efc1070e8a4aba64ce7b3fd0
Mike <mikevanloe@gmail.com>
2015-12-04 13:17:22 +0100

    v0.2 made it look like you hooked a fish

--- DK2_EggShellVR/Assets/Script/ActivationZone.cs
+++ DK2_EggShellVR/Assets/Script/ActivationZone.cs
@@ -4,8 +4,13 @@ using System.Collections;
 public class ActivationZone : MonoBehaviour {
 	public Transform fishingGame;
 
-	void OnTriggerEnter(Collider other)
+	void OnTriggerEnter()
 	{
 		fishingGame.gameObject.SetActive (true);
+	}
+
+	void OnTriggerExit()
+	{
+		fishingGame.gameObject.SetActive (false);
 	}
 }
