7de48b1e5c9b0027e0bd87d567d11a3524e853c9
Mike <mikevanloe@gmail.com>
2015-12-04 11:48:55 +0100

    v0

---
+++ DK2_EggShellVR/Assets/Script/ActivationZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivationZone : MonoBehaviour {
+	public Transform fishingGame;
+
+	void OnTriggerEnter(Collider other)
+	{
+		fishingGame.gameObject.SetActive (true);
+	}
+}
