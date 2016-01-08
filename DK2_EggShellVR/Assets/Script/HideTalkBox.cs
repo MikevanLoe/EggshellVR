474b3c6026bf1efc7608c8a32f16803fa11e6f62
Mike <mikevanloe@gmail.com>
2016-01-08 10:38:02 +0100

    forgotten files

---
+++ DK2_EggShellVR/Assets/Script/HideTalkBox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideTalkBox : MonoBehaviour {
+	public TextMesh Text;
+	public GameObject TextBox;
+	public GameObject TalkIcon;
+	
+	void Update () {
+		TextBox.SetActive (Text.text != "");
+		if (Text.text == "")
+			TalkIcon.SetActive (false);
+	}
+}
